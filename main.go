379408731f5c430850a49b7ddf3845358584f6c2
angelvargass <59036972+angelvargass@users.noreply.github.com>
2025-01-08 22:24:15 -0600

    error handling

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -17,9 +18,14 @@ func main() {
 	}
 
 	client := ssm.NewFromConfig(cfg)
-	res, _ := client.GetParameter(context.TODO(), &ssm.GetParameterInput{
+	res, err := client.GetParameter(context.TODO(), &ssm.GetParameterInput{
 		Name: aws.String("/devopsplayground/hello-world"),
 	})
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 
 	fmt.Println(*res.Parameter.Value)
 }
